17dd8c8fd45d001e2aa52da5958b48e586f5ecc6
@sev4kz <seva200218@gmail.com>
2023-07-05 13:46:56 +0300

    feat: in process of implementing task 2

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,12 @@
 import { HttpStatus } from '@nestjs/common';
+import { ICreateNote } from 'types';
 
 export const URL_PREFIX = 'api';
 
 export enum BASE_URLS {
   GREETINGS = '/greetings/:name?',
+  NOTES = '/notes',
+  NOTES_BY_ID = '/notes/:id?',
 }
 
 export const WRONG_NAME_ERROR = {
@@ -11,4 +14,19 @@ export const WRONG_NAME_ERROR = {
   message: 'Wrong input name',
 };
 
+export const WRONG_POST_NOTE = {
+  code: HttpStatus.BAD_REQUEST,
+  message: 'Wrong note body',
+};
+
 export const PARAM_NAME = 'name';
+
+export const MOCKED_NOTES = [];
+
+export const MOCKED_NOTE: ICreateNote = {
+  id: '',
+  title: '',
+  content: '',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
--- src/notes.controller.ts
+++ src/notes.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, HttpStatus, Param, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { Response } from 'express';
 
 import { NotesService } from 'notes.service';
@@ -8,7 +16,10 @@ import {
   URL_PREFIX,
   WRONG_NAME_ERROR,
   PARAM_NAME,
+  MOCKED_NOTES,
+  WRONG_POST_NOTE,
 } from './constants';
+import { ICreateNote } from 'types';
 
 @Controller(URL_PREFIX)
 export class NotesController {
@@ -26,4 +37,21 @@ export class NotesController {
           .send(this.notesService.getHelloWithName(name))
       : res.status(HttpStatus.BAD_REQUEST).json(WRONG_NAME_ERROR);
   }
+
+  @Get(BASE_URLS.NOTES)
+  getNotes(@Res({ passthrough: true }) response: Response): Response {
+    return response.status(HttpStatus.OK).send(MOCKED_NOTES);
+  }
+
+  @Post(BASE_URLS.NOTES)
+  insertNote(
+    @Body() note: ICreateNote,
+    @Res({ passthrough: true }) response: Response,
+  ): Response {
+    const { id, title, content, createdAt } = note;
+
+    return id && title && content && createdAt
+      ? response.status(HttpStatus.OK).send(note)
+      : response.status(HttpStatus.BAD_REQUEST).json(WRONG_POST_NOTE);
+  } //TODO: остановился на таске 2 надо сделать put, delete + postman переключаюсь на front
 }
