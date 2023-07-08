38f4456c6bfce118b8ea66c9b001d79f3fd59b4a
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-07-08 14:54:00 +0300

    feat: add logger for requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 
+import { LoggerMiddleware } from 'logger/logger.middleware';
 import { NotesModule } from 'notes/notes.module';
 
 @Module({
   imports: [NotesModule],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
