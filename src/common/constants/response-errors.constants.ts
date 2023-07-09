8773192af2b6ac650bb59ae1fba3b27c9cd29dac
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-07-09 19:56:55 +0300

    fix: remove unnecessary messages

--- src/common/constants/response-errors.constants.ts
+++ src/common/constants/response-errors.constants.ts
@@ -1,7 +1,5 @@
 export const enum RESPONSE_ERROR_MESSAGES {
-  WRONG_POST_NOTE = 'Wrong note body',
   WRONG_NAME_ERROR = 'Wrong input name',
-  WRONG_ID = 'Wrong id value',
   ID_NOT_EQUALS = "Id got via params is not equals with body's id",
   BAD_DATABASE_CONNECTION = 'Database connection is lost',
 }
