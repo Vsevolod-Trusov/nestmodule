f25763af17fd479ec365977b900877236ac01261
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-07-09 13:39:13 +0300

    fix: add constants

--- src/common/constants/common.constants.ts
+++ src/common/constants/common.constants.ts
@@ -1,13 +1,12 @@
-import { v4 as uuidv4 } from 'uuid';
-
 import { CreateNoteDto } from 'dto';
 
 export const EMPTY_LINE = '';
+export const UUID_LENGTH = 36;
 
 export const MOCKED_NOTES = [];
 
 export const MOCKED_NOTE: CreateNoteDto = {
-  id: uuidv4(),
+  id: EMPTY_LINE,
   title: EMPTY_LINE,
   content: EMPTY_LINE,
   createdAt: new Date(),
