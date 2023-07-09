a01bc065a69a64ce2c1cf9e7132eb6771c872d39
@sev4kz <vsevolod_trusov_00@mail.ru>
2023-07-09 13:43:37 +0300

    fix: remove unnecessary constants

--- src/common/constants/common.constants.ts
+++ src/common/constants/common.constants.ts
@@ -1,19 +1,2 @@
-import { CreateNoteDto } from 'dto';
-
 export const EMPTY_LINE = '';
 export const UUID_LENGTH = 36;
-
-export const MOCKED_NOTES = [];
-
-export const MOCKED_NOTE: CreateNoteDto = {
-  id: EMPTY_LINE,
-  title: EMPTY_LINE,
-  content: EMPTY_LINE,
-  createdAt: new Date(),
-  updatedAt: new Date(),
-};
-
-export const SUCCESS_DELETED = {
-  _id: EMPTY_LINE,
-  success: true,
-};
