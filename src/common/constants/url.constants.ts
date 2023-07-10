f65eabf7676b860db6e8e272750b9544ccc6496f
@sev4kz <seva200218@gmail.com>
2023-07-10 15:46:06 +0300

    fix: rename url enum for notes and add url for user controller

--- src/common/constants/url.constants.ts
+++ src/common/constants/url.constants.ts
@@ -1,9 +1,16 @@
 export const URL_PREFIX = 'api';
 
-export enum BASE_URLS {
+export enum BASE_NOTES_URLS {
   GREETINGS = '/greetings/:name?',
   NOTES = '/notes',
   NOTES_BY_ID = '/notes/:id?',
+}
+
+export enum BASE_USER_URLS {
+  SIGN_UP = '/sign-up',
+  SIGN_IN = '/sign-in',
+  LOG_OUT = '/log-out',
+  REFRESH = '/refresh',
 }
 
 export const ALL_ROUTES = '*';
