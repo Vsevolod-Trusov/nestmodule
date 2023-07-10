e45584262a467d57665c15358c106e146cde7e7c
@sev4kz <seva200218@gmail.com>
2023-07-10 11:45:09 +0300

    fix: delete unnecessary import

--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Note, NoteSchema } from 'entity';
 
 import { NotesController } from 'notes/notes.controller';
 import { NotesService } from 'notes/notes.service';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: Note.name, schema: NoteSchema }]),
-  ],
   controllers: [NotesController],
   providers: [NotesService],
 })
