8ed4a17873241e7a6ea7e58007fa4243ac526a25
@sev4kz <seva200218@gmail.com>
2023-07-10 11:42:43 +0300

    feat: implement config module and add env file

--- src/data-service/data-service.module.ts
+++ src/data-service/data-service.module.ts
@@ -1,19 +1,25 @@
 import { Global, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
 
 import { Note, NoteSchema, User, UserSchema } from 'entity';
 import { DataService } from 'types';
-
+import { ENV } from 'common';
 import { MongoDataService } from './mongo.data-service';
 
 @Global()
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Note.name, schema: NoteSchema }, 
-                               { name: User.name, schema: UserSchema }]),
-    MongooseModule.forRoot(
-      'mongodb+srv://user:Seva2002@cluster0.mgfazyl.mongodb.net/',
-    ),
+    MongooseModule.forFeature([
+      { name: Note.name, schema: NoteSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>(ENV.DATABASE_URL),
+      }),
+      inject: [ConfigService],
+    }),
   ],
   providers: [
     {
