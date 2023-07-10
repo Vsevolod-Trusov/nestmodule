0b7d6d9481e948bf74da6f0989eeca838a2aa8c1
@sev4kz <seva200218@gmail.com>
2023-07-10 11:44:03 +0300

    fix: add constants for headers and env file

--- src/common/constants/common.constants.ts
+++ src/common/constants/common.constants.ts
@@ -2,5 +2,6 @@ export const EMPTY_LINE = '';
 export const UUID_LENGTH = 36;
 export const TASK_MIN_LENGTH = 3;
 export const TASK_MAX_LENGTH = 500;
+export const PASSWORD_MAX_LENGTH = 2002;
 export const DATABASE_READY_STATE = 1;
 export const MIN_DATE_VALUE = '1970-01-01';
--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -1,3 +1,5 @@
 export * from './common.constants';
 export * from './url.constants';
 export * from './response-errors.constants';
+export * from './env.constants';
+export * from './header-names.constants';
