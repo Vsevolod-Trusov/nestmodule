8d74ec693032d2975bdbbd02ea22b5afc0b90497
@sev4kz <seva200218@gmail.com>
2023-07-18 11:25:43 +0300

    feat: implement all exceptions filter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+
+import { AllExceptionsFilter } from 'middleware/errors.middleware';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: true }));
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
